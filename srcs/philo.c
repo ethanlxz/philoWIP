e2cb900aa062d58ad650641253e00e953bf57f84
ethanlxz <ethanlxz@gmail.com>
2023-07-06 22:24:21 +0800

    4

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,13 +6,13 @@
 /*   By: etlaw <ethanlxz@gmail.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 20:49:23 by etlaw             #+#    #+#             */
-/*   Updated: 2023/07/06 21:13:28 by etlaw            ###   ########.fr       */
+/*   Updated: 2023/07/06 22:24:06 by etlaw            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-void	create_philo(int id, t_info *info)
+int	create_philo(int id, t_info *info)
 {
 
 }
